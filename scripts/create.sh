e62c42b93b624a99dd87541d940d6bb9c749880e
wasuken <wasuken1@gmail.com>
2020-12-08 00:45:14 +0900

    commit.

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+nowtime=`date "+%Y-%m-%d"`
+path="./"
+echo "created ${path}${nowtime}.md"
+echo "# 日報(${nowtime})" >> "${path}${nowtime}.md"
+
+declare -a titles=("今日やったこと" "明日やること")
+
+title=""
+id_base=0
+echo "" >> "${path}${nowtime}.md"
+echo "## 今日やったこと" >> "${path}${nowtime}.md"
+
+let id_base=$#+1
+echo "" >> "${path}${nowtime}.md"
+echo "## 明日やること" >> "${path}${nowtime}.md"
